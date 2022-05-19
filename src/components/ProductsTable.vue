c285b9c28c28a0a26f5a68bf8d67d52f65fd6ce3
kirilldonenko <kirill.donenko@gmail.com>
2022-05-19 18:02:44 +0300

    add test task files

---
+++ src/components/ProductsTable.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <v-btn
+      class="btn-download"
+      @click="downloadFile"
+    >
+      Download
+    </v-btn>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left">
+            ID
+          </th>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Price
+          </th>
+          <th class="text-left">
+            Is package
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="product in products"
+            :key="product.id"
+        >
+          <td>{{ product.id }}</td>
+          <td>{{ product.name }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.isPackage }}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductsTable",
+  data: () => ({
+    products: [],
+  }),
+  async mounted () {
+    await fetch('https://raw.githubusercontent.com/OlegFomishyn/test-data/main/products.json').then(res => res.json()).then(data => this.products = data);
+    this.flatProducts(this.products);
+    this.products = this.uniqueArray(this.products);
+  },
+  methods: {
+    flatProducts (arr) {
+      let recursionNeed = false;
+      arr.forEach((item, index) => {
+        if (typeof item.included === 'object') {
+          arr.splice(index + 1, 0, ...item.included);
+          item.included = item.isPackage = 'Yes';
+          recursionNeed = true;
+        } else if (!('included' in item)) {
+          item.isPackage = 'No';
+        }
+        item.price = typeof item.price === 'string' ? item.price : ('$' + Number(item.price) / 100);
+      })
+      if (recursionNeed) {
+        this.flatProducts(arr);
+      }
+    },
+    uniqueArray (a) {
+      return [...new Set(a.map(o => JSON.stringify(o)))].map(s => JSON.parse(s));
+    },
+    downloadFile () {
+      let date = new Date();
+      date = date.getDate() + '_' + date.getMonth() + '_' + date.getFullYear();
+      this.products.map((product) => {
+        delete product.included;
+        delete product.isPackage;
+        return product;
+      })
+      const data = JSON.stringify(this.products);
+      const blob = new Blob([data], {type: 'text/plain'});
+      let a = document.createElement('a');
+      a.download = `${date}.json`;
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+      a.click();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .btn-download {
+    margin: 20px;
+  }
+</style>
